0a2d1f1f2c4ebfbbfb7a1095e5d15f3f6dd12784
DavidDGamble <daviddgamble@gmail.com>
2023-02-11 16:56:19 -0800

    Add all code for react with redux example.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+//vvvvv---Added for Redux---vvvvv
+// import { createStore } from 'redux';
+import { legacy_createStore as createStore} from 'redux'
+import reducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
+
+// Great way to test by keeping an eye on the current state of store
+store.subscribe(() => 
+  console.log(store.getState())
+);
+//^^^^^---Added for Redux---^^^^^
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* vvvvv---Added for Redux---vvvvv */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+    {/* ^^^^^---Added for Redux---^^^^^ */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
